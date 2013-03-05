3b11359710b522f3daca9c25e9a9c2838f37c9c7
Candas Sekerci <candas@wiggledigital.com>
2013-03-05 18:44:12 +0200

    ana  menüye employees ve department geldi

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,6 +32,8 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+                array('label'=>'Departments', 'url'=>array('/department')),
+                array('label'=>'Employees', 'url'=>array('/employee')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
